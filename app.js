0e50e7d487478e4f4e53daf8c3f5555d0cb1615f
KelvinLeeder <kleeder77@outlook.com>
2022-05-18 10:20:50 -0500

    Sea gregan rutas para que reciba Query Params

--- app.js
+++ app.js
@@ -16,6 +16,20 @@ app.get('/launchx', (req, res) =>{
     res.send("Bienvenidos a launchx")
 })
 
+//Regresando un objeto
+//localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) =>{
+    const explorer = {name: 'Explorer', msg: 'Hello'}
+    res.send(explorer)
+})
+
+//Query Params: Recibir parametros por la url
+//http://localhost:3000/explorers/carlo
+//req.params = {"explorerName": "Kelvin"}
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
+
 //Con esto inicializamos esta app
 app.listen(port, () =>{
     console.log(`Example de app listening on port`)
