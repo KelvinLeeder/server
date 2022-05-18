648d54736818c9298fa771d1fe47f6897dbdb390
KelvinLeeder <kleeder77@outlook.com>
2022-05-18 09:55:22 -0500

    Se agrega el archivp app.js para llamar al server

---
+++ app.js
@@ -0,0 +1,17 @@
+//Usando un objeto de express
+const express = require('express')
+//App de express
+const app = express()
+//Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000 //local host
+
+//Path iniciar, repondera a la url localhost: 3000
+
+app.get('/', (req, res) =>{
+    res.send("¡Hello World!")
+})
+
+//Con esto inicializamos esta app
+app.listen(port, () =>{
+    console.log("Example de app listening on port")
+})
